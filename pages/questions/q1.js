3b08314034e717629760e47e67c4143c2aea4108
Oliver Anyanwu <oliver.anyanwu@aol.com>
2022-09-15 15:21:56 +0100

    style

--- pages/questions/q1.js
+++ pages/questions/q1.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link';
-import styles from '/oliver-product-advocate-test1/styles/Home.module.css'
+import styles from '../styles/globals.css'
 
 export default function FirstQuestion() {
   return (
