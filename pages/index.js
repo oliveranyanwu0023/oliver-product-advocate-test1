83db72afecace7876d43eda778c123dbfd8f4884
Oliver Anyanwu <oliver.anyanwu@aol.com>
2022-09-15 14:41:12 +0100

    css and grid

--- pages/index.js
+++ pages/index.js
@@ -21,27 +21,31 @@ export default function Home() {
           Product Advocate Position at Vercel: Skills Test
         </p>
 
-        <div className={styles.card}>
+        <div className={styles.grid}>
 
-          <Link href="/questions/q1" className={styles.card}>
-            <h2>1. Things a Product Advocate Might Do &rarr;</h2>
-          </Link>
+          <div className={styles.card}>
 
-          <a href="" className={styles.card}>
-            <h2>2. Reflection on past experience &rarr;</h2>
-          </a>
+            <Link href="/questions/q1">
+              <h2>1. Things a Product Advocate Might Do &rarr;</h2>
+            </Link>
 
-          <a href=""className={styles.card}>
-            <h2>3. Using Next.js &rarr;</h2>
-          </a>
+            <a href="" >
+              <h2>2. Reflection on past experience &rarr;</h2>
+            </a>
 
-          <a href="" className={styles.card}>
-            <h2>4. Practice Email Responses &rarr;</h2>
-          </a>
+            <a href="">
+              <h2>3. Using Next.js &rarr;</h2>
+            </a>
 
-          <a href=""className={styles.card}>
-            <h2>5. Your personal recommendations &rarr;</h2>
-          </a>
+            <a href="" >
+              <h2>4. Practice Email Responses &rarr;</h2>
+            </a>
+
+            <a href="">
+              <h2>5. Your personal recommendations &rarr;</h2>
+            </a>
+
+          </div>
 
         </div>
       </main>
