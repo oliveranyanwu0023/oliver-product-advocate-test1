f3512dbd54807f87f53e88872dacdd5786056094
Oliver Anyanwu <oliver.anyanwu@aol.com>
2022-09-15 15:08:12 +0100

    first question page content

--- pages/questions/q1.js
+++ pages/questions/q1.js
@@ -1,9 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link';
+import styles from '../styles/Home.module.css'
 
 export default function FirstQuestion() {
   return (
     <>
-      <h1>First First</h1>
+      <h1>Things a Product Advocate Might Do</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
