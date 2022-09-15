f01553b90fc2e52e3e500056f7c762be0990f981
Oliver Anyanwu <oliver.anyanwu@aol.com>
2022-09-15 15:57:12 +0100

    ...

--- pages/questions/q1.js
+++ pages/questions/q1.js
@@ -41,24 +41,6 @@ export default function FirstQuestion() {
         </a>
       </footer>
     </div>
-    <>
-      <h1>Things a Product Advocate Might Do</h1>
-
-      <h2>
-
-      Most Interested:
-
-        - Write direct and concise emails that dig into a prospect&apos;s pain points and needs
-        - Hop on a Zoom call to qualify a prospect or educate them about Vercel
-        - Organize, Schedule & book meetings for Account Executives with prospects
-
-        Least Interested:
-
-        - Research the headquarters & ownership of a company to determine who to route it to
-        - Evaluate an application to help determine if it&apos;s commercial or not
-        - Evaluate OSS sponsorship applications
-       
-      </h2>
 
       <h2>
         <Link href="/">Return to Main Page</Link>
